f36d433b0daf62afe65b374aa36f95470e23625d
Dayna <Daynaa.clifford@gmail.com>
2023-02-09 22:05:26 -0800

    Hopfully this works

---
+++ daycliff/src/App.js
@@ -0,0 +1,17 @@
+import Harper from './Harper.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Harper} className="App-logo" alt="logo" />
+        <p>
+          <code>Coming sooonnnn</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
